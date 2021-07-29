de5ed14b6b481abbcd407e83f0a3ba638a9db849
andrew quenette <and1roxx43@gmail.com>
2021-07-29 16:26:35 +1000

    Modified client App.css App.js components

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './App.css';
 import Home from "./pages/Home";
 import Login from "./pages/Login";
@@ -7,11 +7,18 @@ import Register from "./pages/Register";
 
 function App() {
   return (
-    <Router>
+
+   
+        <Router>
+            <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/login" component={Login} />
         <Route exact path="/register" component={Register} />
+        </Switch>
     </Router>
+
+   
+    
   );
 }
 
--- client/src/components/Buttons.js
+++ client/src/components/Buttons.js
@@ -1,10 +1,14 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 function Button() {
     return (
-        <div className="btn-cont">
-            <a href="" className="loginBtn btn">Login</a>
-            <a href="" class="registerBtn btn">Register</a>
+        <div>
+            <ul className="btn-container">
+                <li className="login-btn"><NavLink className="btn" to="/Login">Login</NavLink></li>
+                <li className="register-btn"><NavLink className="btn" to="/Register">Register</NavLink></li>
+            </ul>
+            
         </div>
     )
 }
