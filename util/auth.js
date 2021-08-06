821943a57220ebe73ef81c682adfd8692d547847
andrew quenette <and1roxx43@gmail.com>
2021-08-06 14:22:33 +1000

    Modified models and utils

--- util/auth.js
+++ util/auth.js
@@ -1,18 +1,17 @@
 const jwt = require("jsonwebtoken");
 const { AuthenticationError } = require("apollo-server");
-const { SECRET_KEY} = require("../config/connection");
+const { TOKEN_KEY } = require("../config");
 
 module.exports = (context) => {
 
-    //
+    // context = { ...headers }
     const authHeader = context.req.headers.authorization;
     if(authHeader) {
+        // convention for tokens: "Bearer ..."
        const token = authHeader.split("Bearer ")[1]; 
-
-       if(token){
-
-        try{
-            const user = jwt.verify(token, SECRET_KEY);
+        if(token){
+            try{
+            const user = jwt.verify(token, TOKEN_KEY);
             return user;
         } catch(err) {
             throw new AuthenticationError("Invalid/Expired token");
