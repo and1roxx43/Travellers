c5af43c974b4cf905cf261f25bc86ff1fceca525
andrew quenette <and1roxx43@gmail.com>
2021-08-10 01:30:36 +1000

    seventh trial

--- server.js
+++ server.js
@@ -25,6 +25,10 @@ server.applyMiddleware({ app })
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+  }
+
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "index.html"));
 })
