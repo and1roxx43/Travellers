3d465a3580b3aa210042cf028fff4d5d0223ca1b
andrew quenette <and1roxx43@gmail.com>
2021-08-10 01:44:04 +1000

    seventh trial

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ if (process.env.NODE_ENV === 'production') {
   }
 
 app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "public", "index.html"));
+    res.sendFile(path.join(__dirname, "public", "index.html"));
 })
 
 
