ee4c92f4fcc47fc298359d152138de3362b0cebc
Andrew Quenette <and1roxx43@gmail.com>
2021-08-09 12:18:13 +1000

    Update Like.js

--- client/src/components/LikeButton.js
+++ client/src/components/LikeButton.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { useMutation } from '@apollo/client';
-import gql from 'graphql-tag';
+import { useMutation, gql } from '@apollo/client';
 import { Button, Icon, Label } from 'semantic-ui-react';
 import Tooltip from '../util/Tooltip';
 
